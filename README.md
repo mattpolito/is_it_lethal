7efd66d0f131d4846ff665ce40943062963e85f0
Matt Polito <matt.polito@gmail.com>
2012-07-01 02:58:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Is It Lethal?
+
+This project came to me one day when my wife and daughers frantically called me wanting to know if a giant bug that had flown in the house was going to bite them.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IsItLethal::Application.config.secret_token = '9237e220d0adc1771d89fe0245b3161b04679c2cb19806f54ee8f680168082cdaf8b5cb84e281a638218c4611e04e06690b28d59ce5e03e0580c3832ae0a25cf'
